ed69c5a0098966a7f58b724ee18165c6b17d4670
k9abhinav <k9abhinava@gmail.com>
2023-12-04 23:43:49 +0530

    GOD

--- old.js
+++ old.js
@@ -59,9 +59,9 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   grabCursor: true,
   loop: true,
-  // autoplay:{
-  //   delay:1500
-  // },
+  autoplay:{
+    delay:2500
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -80,52 +80,6 @@ var swiper = new Swiper(".mySwiper", {
 // 
 
 
-// COUNTERRRRRRRRRRRRRRRR
 
-const workSection = document.querySelector(".section-work-data");
-const workObserver = new IntersectionObserver(
-(entries, observer) => {
-  const [entry] = entries;
-  // console.log(entry);
-
-  // if (entry.isIntersecting == false)
-  if (!entry.isIntersecting) return;
-
-  // animate number counter
-
-  const counterNum = document.querySelectorAll(".counter-numbers");
-
-  const speed = 200;
-
-  counterNum.forEach(curElem => {
-    const updateNumber = () => {
-      const targetNumber = parseInt(curElem.dataset.number);
-      // console.log(targetNumber);
-      const initialNum = parseInt(curElem.innerText);
-      // console.log(initialNum);
-
-      const incrementNumber = Math.trunc(targetNumber / speed);
-      // console.log(incrementNumber);
-
-      if (initialNum < targetNumber) {
-        curElem.innerText = `${initialNum + incrementNumber}+`;
-        setTimeout(updateNumber, 10);
-      }
-    };
-
-    updateNumber();
-  });
-
-  observer.unobserve(workSection);
-},
-{
-  root: null,
-  threshold: 0 });
-
-
-
-workObserver.observe(workSection);
-
-// COUNTER ANIMATTTTIONNNNNNNNNNNNNNNN ENDDDDDDDD
 
 
